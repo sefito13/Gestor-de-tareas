a8a824d50dc92906762927e9c629b81f44d18737
Sebastian Eduardo Fandiño Orjuela <sebastianfandino13@gmail.com>
2026-07-09 00:31:29 -0500

    Correccion Carga Proyectos

--- Gestor V1/models/tarea.py
+++
@@ -1,7 +0,0 @@
-class Tarea:
-    def __init__(self, titulo, estado="Pendiente"):
-        self.titulo = titulo
-        self.estado = estado
-
-    def completar(self):
-        self.estado = "Completada"
