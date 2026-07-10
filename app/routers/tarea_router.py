c6502a67908f64b0702d6a3887ebabf070512403
Sebastian Eduardo Fandiño Orjuela <sebastianfandino13@gmail.com>
2026-07-10 00:02:35 -0500

    Creacion Crud completo

--- app/routers/tarea_router.py
+++ app/routers/tarea_router.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
 from app.models.tarea import Tarea
-from app.schemas.tarea import TareaCreate
+from app.schemas.tarea import TareaCreate, TareaUpdate
 
 def get_db():
     db = SessionLocal()
@@ -30,3 +30,62 @@ def crear_tarea(
     db.refresh(nueva_tarea)
     
     return nueva_tarea
+
+@router.get("/")
+def obtener_tareas(
+    db: Session = Depends(get_db)
+):
+    tareas = db.query(Tarea).all()
+    return tareas
+
+@router.get("/{tarea_id}")
+def obtener_tarea(
+    tarea_id: int,
+    db: Session = Depends(get_db)
+):
+    tarea = db.query(Tarea).filter(Tarea.id == tarea_id).first()
+    if not tarea:
+        raise HTTPException(
+            status_code=404,
+            detail="Tarea no encontrada"
+        )
+    
+    return tarea
+
+@router.put("/{tarea_id}")
+def actualizar_tarea(
+    tarea_id:int,
+    tarea: TareaUpdate,
+    db: Session = Depends(get_db)
+):
+    tarea_existente = db.query(Tarea).filter(Tarea.id == tarea_id).first()
+    if not tarea_existente:
+        raise HTTPException(
+            status_code=404,
+            detail="Tarea no encontrada"
+        )
+    
+    tarea_existente.titulo = tarea.titulo
+    tarea_existente.estado = tarea.estado.value
+
+    db.commit()
+    db.refresh(tarea_existente)
+
+    return tarea_existente
+
+@router.delete("/{tarea_id}")
+def eliminar_tarea(
+    tarea_id: int,
+    db: Session = Depends(get_db)
+):
+    tarea_existente = db.query(Tarea).filter(Tarea.id == tarea_id).first()
+    if not tarea_existente:
+        raise HTTPException(
+            status_code=404,
+            detail="Tarea no encontrada"
+        )
+    
+    db.delete(tarea_existente)
+    db.commit()
+
+    return {"message": "Tarea eliminada con exito"}
--- app/schemas/tarea.py
+++ app/schemas/tarea.py
@@ -1,8 +1,20 @@
 from pydantic import BaseModel, Field
+from enum import Enum
 
-class TareaCreate(BaseModel):
+class EstadoTarea(str, Enum):
+    pendiente = "Pendiente"
+    en_curso = "En Curso"
+    completada = "Completada"
+
+class TareaBase(BaseModel):
     titulo: str = Field(
         ..., 
         min_length=3, 
         max_length=100
     )
+
+class TareaCreate(TareaBase):
+    pass
+
+class TareaUpdate(TareaBase):
+    estado: EstadoTarea
