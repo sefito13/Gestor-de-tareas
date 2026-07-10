8dc405eb63b6b45237f2b268237a567a2a0f5671
Sebastian Eduardo Fandiño Orjuela <sebastianfandino13@gmail.com>
2026-07-10 01:40:07 -0500

    Implementacion Repository

--- app/services/tarea_services.py
+++ app/services/tarea_services.py
@@ -1,40 +1,18 @@
 from sqlalchemy.orm import Session
-from app.models.tarea import Tarea
 from app.schemas.tarea import TareaCreate, TareaUpdate
+from app.repositories import tarea_repository
 
 def crear_tarea(db: Session, tarea: TareaCreate):
-    nueva_tarea = Tarea(
-        titulo=tarea.titulo
-    )
-
-    db.add(nueva_tarea)
-    db.commit()
-    db.refresh(nueva_tarea)
-
-    return nueva_tarea
+    return tarea_repository.crear_tarea(db, tarea)
 
 def obtener_tareas(db: Session):
-    return db.query(Tarea).all()
+    return tarea_repository.obtener_tareas(db)
 
 def obtener_tarea(db: Session, tarea_id: int):
-    return db.query(Tarea).filter(Tarea.id == tarea_id).first()
+    return tarea_repository.obtener_tarea(db, tarea_id)
 
 def actualizar_tarea(db: Session, tarea_id: int, tarea: TareaUpdate):
-    tarea_existente = obtener_tarea(db, tarea_id)
-    if not tarea_existente:
-        return None
-    
-    tarea_existente.titulo = tarea.titulo
-    tarea_existente.estado = tarea.estado
-    db.commit()
-    db.refresh(tarea_existente)
-    return tarea_existente
+    return tarea_repository.actualizar_tarea(db, tarea_id, tarea)
 
 def eliminar_tarea(db: Session, tarea_id: int):
-    tarea_existente = obtener_tarea(db, tarea_id)
-    if not tarea_existente:
-        return None
-    
-    db.delete(tarea_existente)
-    db.commit()
-    return tarea_existente
+    return tarea_repository.eliminar_tarea(db, tarea_id)
