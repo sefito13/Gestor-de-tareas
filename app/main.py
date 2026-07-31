b3e84cfb021dd6c9812fefc818f5d72047f4f529
Sebastian Fandiño <sebastianfandino13@gmail.com>
2026-07-31 13:40:10 -0500

    Final arquitectura y revision backend

--- app/main.py
+++ app/main.py
@@ -3,7 +3,26 @@ from app.routers.tarea import router as tarea_router
 from app.routers.usuario import router as usuario_router
 from app.routers.auth import router as auth_router
 
-app = FastAPI()
+openapi_tags = [
+    {
+        "name": "Auth", 
+        "description": "Autenticacion"
+    },
+    {
+        "name": "Usuarios", 
+        "description": "Gestion de usuarios"
+    },
+    {
+        "name": "Tareas", 
+        "description": "Gestion de tareas"
+    }
+]
+
+app = FastAPI(
+    title="Task Manager API",
+    description="API para gestion de tareas",
+    version="1.0.0"
+)
 
 app.include_router(tarea_router)
 app.include_router(usuario_router)
